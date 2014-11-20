a90a50df3824febb8c504beae6c710554c5dca3d
Paul Heijmans <paulheijmans@MacBook-Pro-van-Paul-2.local>
2014-11-20 21:09:55 +0100

    Achtergrond, logo image Design logo, registratie en login button & or tussen de buttons Achtergrond andere views (login & registratie) UItabItems Geena achtergrond en geen tekst Register & Login controller aangemaakt

---
+++ OuiOui/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  OuiOui
+//
+//  Created by Paul Heijmans on 20-11-14.
+//  Copyright (c) 2014 Vontura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UINavigationItem *navigationBar;
+
+@end
---
+++ OuiOui/RegisterViewController.h
@@ -0,0 +1,14 @@
+//
+//  RegisterViewController.h
+//  OuiOui
+//
+//  Created by Paul Heijmans on 20-11-14.
+//  Copyright (c) 2014 Vontura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UINavigationItem *navigationBar;
+
+@end
