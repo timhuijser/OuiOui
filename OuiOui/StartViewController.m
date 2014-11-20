47b52153210752fa1d3cfaeb3e024c0ecbbdcee5
Paul Heijmans <paulheijmans@MacBook-Pro-van-Paul-2.local>
2014-11-20 15:17:54 +0100

    Navigation controller

---
+++ OuiOui/StartViewController.m
@@ -0,0 +1,49 @@
+//
+//  StartViewController.m
+//  OuiOui
+//
+//  Created by Paul Heijmans on 20-11-14.
+//  Copyright (c) 2014 Vontura. All rights reserved.
+//
+
+#import "StartViewController.h"
+
+@interface StartViewController ()
+
+@end
+
+@implementation StartViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    self.navigationController.navigationBar.hidden = YES;
+    self.navigationController.navigationItem.hidesBackButton = YES;
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:YES animated:animated];
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    [super viewWillDisappear:animated];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
