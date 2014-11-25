5f06e01c8892f002b7d03a49311f5e66fab6ccba
Tim Huijser <timhuijser@gmail.com>
2014-11-25 19:55:41 +0100

    Restored noirmal and FB signup.

--- OuiOui/RegisterViewController.m
+++ OuiOui/RegisterViewController.m
@@ -58,6 +58,91 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)signupButton:(id)sender {
+    
+    NSString *username = [self.email text];
+    NSString *name = [self.name text];
+    NSString *email = [self.email text];
+    NSString *password = [self.password text];
+    
+    if ([username length] < 4 || [password length] < 4) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Invalid Entry" message:@"Username and Password must both be at least 4 characters long." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+        [alert show];
+    } else if ([email length] < 8) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Invalid Entry" message:@"Please enter your email address." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+        [alert show];
+    } else {
+        
+        PFUser *newUser = [PFUser user];
+        newUser.username = username;
+        newUser.password = password;
+        newUser.email = email;
+        newUser[@"name"] = name;
+        
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (error) {
+                NSString *errorString = [[error userInfo] objectForKey:@"error"];
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+                [alert show];
+            } else {
+                UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Tab" bundle:nil];
+                UITabBarController *obj=[storyboard instantiateViewControllerWithIdentifier:@"tab"];
+                self.navigationController.navigationBarHidden=YES;
+                [self.navigationController pushViewController:obj animated:YES];
+            }
+        }];
+    }
+}
+
+- (IBAction)signupFacebookButton:(id)sender {
+    
+    NSArray *permissions = @[@"email"];
+    
+    [PFFacebookUtils logInWithPermissions:permissions block:^(PFUser *user, NSError *error) {
+        if (!user) {
+            NSLog(@"Uh oh. The user cancelled the Facebook login.");
+        } else if (user.isNew) {
+            
+            FBRequest *request = [FBRequest requestForMe];
+            [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+                if (!error) {
+                    // result is a dictionary with the user's Facebook data
+                    NSDictionary *userData = (NSDictionary *)result;
+                    
+                    NSString *name = userData[@"name"];
+                    NSString *email = userData[@"email"];
+                    NSString *facebookID = userData[@"id"];
+                    
+                    user.email = email;
+                    user[@"name"] = name;
+                    user[@"facebookID"] = facebookID;
+                    [user save];
+                    
+                    /* TODO: Retrieve pictureURL and store it with Parse.
+                     NSURL *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
+                     
+                     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:pictureURL];
+                     
+                     // Run network request asynchronously
+                     [NSURLConnection sendAsynchronousRequest:urlRequest
+                     queue:[NSOperationQueue mainQueue]
+                     completionHandler:
+                     ^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                     if (connectionError == nil && data != nil) {
+                     // Set the image in the header imageView
+                     self.headerImageView.image = [UIImage imageWithData:data];
+                     }
+                     }];
+                     */
+                }
+            }];
+            
+            NSLog(@"User signed up and logged in through Facebook!");
+        } else {
+            NSLog(@"User logged in through Facebook!");
+        }
+    }];
+}
 
 /*
  #pragma mark - Navigation
